c84877ce0d4c65521dbf5406d7656941aaf80821
hunard <hunarwork11@gmail.com>
2026-07-01 01:24:24 +0530

    arithmetic circuits and kmaps to circuit

---
+++ circuits/combinational logic/Arithmetic circuits/q4j.v
@@ -0,0 +1,28 @@
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output cout,
+    output sum
+);
+
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+
+endmodule
+
+
+module top_module (
+    input [3:0] x,
+    input [3:0] y,
+    output [4:0] sum
+);
+
+    wire c1, c2, c3;
+
+    full_adder FA0 (.a(x[0]), .b(y[0]), .cin(1'b0), .cout(c1),     .sum(sum[0]));
+    full_adder FA1 (.a(x[1]), .b(y[1]), .cin(c1),   .cout(c2),     .sum(sum[1]));
+    full_adder FA2 (.a(x[2]), .b(y[2]), .cin(c2),   .cout(c3),     .sum(sum[2]));
+    full_adder FA3 (.a(x[3]), .b(y[3]), .cin(c3),   .cout(sum[4]), .sum(sum[3]));
+
+endmodule
